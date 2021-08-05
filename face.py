5749c07166ba950e2abfcda8e95b2e33cbecd694
jithendra pulivarthi <67385938+jithendra795@users.noreply.github.com>
2021-08-05 19:32:24 +0530

    Update face.py

--- face.py
+++ face.py
@@ -1,34 +1,14 @@
 import cv2
-
-# Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# To capture video from webcam. 
 cap = cv2.VideoCapture(0)
-# To use a video file as input 
-# cap = cv2.VideoCapture('filename.mp4')
-
 while True:
-    # Read the frame
     _, img = cap.read()
-
-    # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    # Display
+    for (d, n, s, j) in faces:
+        cv2.rectangle(img, (d, n), (d+s, n+j), (255, 0, 0), 2)
     cv2.imshow('img', img)
-
-    # Stop if escape key is pressed
     k = cv2.waitKey(10) & 0xff
-    if k==2:
+    if j==2:
         break
-        
-# Release the VideoCapture object
 cap.release()
